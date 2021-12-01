34b8a0fa5e27e425012696109cd0cc6c42d7f72c
Sasank <spalamangalam@gmail.com>
2021-12-01 13:56:55 +0530

    upstac

---
+++ src/main/java/org/upgrad/upstac/testrequests/CreateTestRequest.java
@@ -0,0 +1,27 @@
+package org.upgrad.upstac.testrequests;
+
+import lombok.Data;
+import org.upgrad.upstac.users.models.Gender;
+
+@Data
+
+public class CreateTestRequest {
+
+
+
+    private String name;
+    private Gender gender;
+    private String address;
+    private Integer age;
+
+    private String email;
+
+    private String phoneNumber;
+
+
+    private Integer pinCode;
+
+
+
+
+}
